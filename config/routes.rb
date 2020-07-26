1ac3d54e686a1a4f8d46bd0015a8dcde583279ff
Trinh997 <trathitrinh07031997@gmil.com>
2020-07-27 00:01:06 +0700

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  get 'users/new'
+  get 'static_pages/home'
+  get 'static_pages/help'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
