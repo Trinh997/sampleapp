2ff2c9308aa7b4703f765daa1ebb69cc3a738a63
Trinh997 <trathitrinh07031997@gmil.com>
2020-07-29 07:52:41 +0700

    chapter 7

--- Gemfile
+++ Gemfile
@@ -2,10 +2,11 @@ source "https://rubygems.org"
 git_source(:github){|repo| "https://github.com/#{repo}.git"}
 
 ruby "2.7.1"
-gem "bcrypt", "3.1.14"
+gem "bcrypt", "~> 3.1.7"
 gem "bootsnap", ">= 1.4.2", require: false
 gem "bootstrap-sass", "3.4.1"
 gem "config"
+gem "jquery-rails", "4.3.1"
 gem "puma", "~> 4.1"
 gem "rails", "~> 6.0.3", ">= 6.0.3.2"
 gem "sass-rails", ">= 6"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,14 @@
 class User < ApplicationRecord
-
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
 
   validates :name, presence: true,
-    length: {maximum: Settings.name.max_length}
+            length: {maximum: Settings.name.max_length}
   validates :email, presence: true,
-    length: {maximum: Settings.email.max_length},
-    format: {with: VALID_EMAIL_REGEX},
-    uniqueness: true
+            length: {maximum: Settings.email.max_length},
+            format: {with: VALID_EMAIL_REGEX},
+            uniqueness: true
   validates :password,
-    length: {minimum: Settings.password.min_length}
-
+            length: {minimum: Settings.password.min_length}
   before_save{email.downcase!}
 
   has_secure_password
