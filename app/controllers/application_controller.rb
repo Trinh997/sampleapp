5a4503aa74604a0fad11d8328df27ef01a0d60e0
Trinh997 <trathitrinh07031997@gmil.com>
2020-07-29 07:52:41 +0700

    chapter 7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,9 @@ class ApplicationController < ActionController::Base
   def default_url_options
     {locale: I18n.locale}
   end
+
+  def extract_locale
+    parsed_locale = params[:locale]
+    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+  end
 end
