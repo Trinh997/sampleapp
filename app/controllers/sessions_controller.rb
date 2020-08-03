8d58ec0410258db439217ade7e94c33755878b76
Trinh997 <trathitrinh07031997@gmil.com>
2020-08-03 16:08:10 +0700

    CHapter 9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,11 @@
 class SessionsController < ApplicationController
-  def new
-    render new
-  end
+  def new; end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user &.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password])
       log_in user
+      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
       redirect_to user
     else
       flash.now[:danger] = t "login.error"
